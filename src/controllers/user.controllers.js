c13405959fe65cb79fc3d06ee600f1cb2b786ac2
aj-1729 <ashwinijhadps@gmail.com>
2026-05-22 04:38:34 +0530

    added email verify method

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -19,7 +19,7 @@ const registerUser = asyncHandler(
 
         const verificationToken = crypto.randomBytes(20).toString('hex');
 
-        const user = await User.create(
+        const user = await User(
             {
                 username,
                 email,
@@ -29,6 +29,8 @@ const registerUser = asyncHandler(
             }
         );
 
+        await user.save();
+
         if(!user)
             throw new ApiError(400, "User data invalid");
 
@@ -37,12 +39,38 @@ const registerUser = asyncHandler(
 
 
         return res.status(200).json(
-            new Apiresponse(202,"User registered!!, check the terminal for verification link!",
-                {
-                    user
-                }
+            new Apiresponse(202,"User registered!!, check the terminal for verification link!",{user}
             )
                  
         )
     }
 )
+
+const verifyEmail = asyncHandler(
+    async(req,res)=>
+    {
+        const user = await User.findOne(
+            {
+                verificationToken = req.params.token,
+                verificationTokenExpire = {$gt: Date.now()}
+            }
+        )
+
+
+        if(!user)
+            return new ApiError(402, "Invalid token or expired token");
+
+        user.isVerified = true;
+        user.verificationToken = undefined;
+        user.verificationTokenExpire = undefined;
+
+        await user.save();
+
+        res.status(200)
+        .json(
+            new Apiresponse(202, "User created successfully", {user})
+        )
+    }
+)
+
+export{registerUser}
