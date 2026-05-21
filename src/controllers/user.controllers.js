6e6ddb11da57e53d0c7c8e3893f125c3395b22ec
aj-1729 <ashwinijhadps@gmail.com>
2026-05-22 04:22:30 +0530

    added register controller method

---
+++ src/controllers/user.controllers.js
@@ -0,0 +1,48 @@
+import { User } from "../models/users.models";
+import ApiError from "../utils/Apierror";
+import Apiresponse from "../utils/Apiresponse";
+import { asyncHandler } from "../utils/asyncHandler";
+
+
+const registerUser = asyncHandler(
+    async(req,res)=>
+    {
+        const {email,username,password} = req.body;
+        if(email === "" || password === "" || username === "")
+            throw new ApiError(402, "All feilds are requiered!!");
+
+        const userExists = await User.findOne({email:email});
+        
+
+        if(userExists)
+            throw new ApiError(402, "This email already exits");
+
+        const verificationToken = crypto.randomBytes(20).toString('hex');
+
+        const user = await User.create(
+            {
+                username,
+                email,
+                password,
+                verificationToken,
+                verificationTokenExpire: Date.now() + 24 * 60 * 60 * 1000
+            }
+        );
+
+        if(!user)
+            throw new ApiError(400, "User data invalid");
+
+        const verifyUrl = `http://localhost:5000/api/v1/users/verify/${verificationToken}`;
+        console.log(verifyUrl)
+
+
+        return res.status(200).json(
+            new Apiresponse(202,"User registered!!, check the terminal for verification link!",
+                {
+                    user
+                }
+            )
+                 
+        )
+    }
+)
