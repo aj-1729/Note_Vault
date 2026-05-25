fa12f187b674827432938553b2f48e8dcead81a9
aj-1729 <ashwinijhadps@gmail.com>
2026-05-25 15:37:08 +0530

    added notes model

---
+++ src/models/notes.models.js
@@ -0,0 +1,26 @@
+import mongoose, { trusted } from "mongoose";
+
+const noteSchema = new mongoose.Schema(
+    {
+        title:{
+            type: String,
+            required: true 
+        },
+        content:{
+            type: String,
+            required: true
+        },
+        owner:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
+        tags: [
+            {
+                type: String,
+            }
+        ],
+    },{timestamps: true}
+);
+
+export const note = mongoose.model("Notes", noteSchema); 
