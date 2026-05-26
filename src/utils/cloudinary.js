f46c819c5c59234175ddbce6f992258d6ef9b9cf
aj-1729 <ashwinijhadps@gmail.com>
2026-05-27 00:29:53 +0530

    added cloudi. method/utils

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,32 @@
+import {v2 as cloudinary} from 'cloudinary';
+
+import fs from 'fs';
+
+cloudinary.config({ 
+
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET 
+
+});
+
+
+const uploadToCloudinary = async (noteContent)=>
+{
+    try {
+        if(!noteContent) return null;
+
+        const conContent = Buffer.from(noteContent).toString('base64');
+        const dataURI = `data:text/plain;base64,${base64Content}`;
+
+        const response = await cloudinary.uploader.upload(dataURI,{
+            resource_type:"auto",
+            folder: "Note_Vault"
+        });
+
+        return response.secure_url;
+    } catch (error) {
+        console.error("Cloudinary upload failed:", error);
+        throw new ApiError(500, "Failed to upload note to secure storage");
+    }
+}
