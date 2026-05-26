d100318d0a1f856ccb8c885cfd4728506ee55f4c
aj-1729 <ashwinijhadps@gmail.com>
2026-05-27 00:04:32 +0530

    added route

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { verifyJWT } from "../../middlewares/auth.middlewares.js";
-import { createNote, getUserNote } from "../controllers/notes.controllers.js";
+import { createNote, deleteNote, getUserNote, updateNote } from "../controllers/notes.controllers.js";
 
 const router = Router();
 
 router.route("/create").post(verifyJWT,createNote)
 router.route("/all").get(verifyJWT,getUserNote)
-
+router.route("/update/:noteId").get(verifyJWT,updateNote)
+router.route("/delete/:noteId").get(verifyJWT,deleteNote)
 export default router
