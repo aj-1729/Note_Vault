c991f911a9f6ae9b8da6d956b178a686da280aad
aj-1729 <ashwinijhadps@gmail.com>
2026-09-23 02:11:21 +0530

    role add

--- src/models/users.models.js
+++ src/models/users.models.js
@@ -27,6 +27,11 @@ const userSchema = new mongoose.Schema(
             minlength: 6,
            // select: false
         },
+        role: {
+            type: String,
+            enum: ["user", "admin"],
+            default: "user"
+        },
         isVerified: {
             type: Boolean,
             default: false
